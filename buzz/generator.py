43e5660705bb5c83214a01f6985639aef7816e2e
Saubhatta <saubhatta.90@gmail.com>
2019-03-26 08:50:14 -0400

    Changed buzz word

--- buzz/generator.py
+++ buzz/generator.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import random
 
 buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
+    'continuous deployment', 'continuous improvement', 'devops', 'foo-bar')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
