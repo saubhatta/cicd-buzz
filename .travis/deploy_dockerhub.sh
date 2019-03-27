c5c14d30e88bcea6d047f945d9de9ccb84a70fcb
Saubhatta <saubhatta.90@gmail.com>
2019-03-27 03:41:06 -0400

    Changed deploy_dockerhub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -6,6 +6,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-echo $TRAVIS_REPO_SLUG
+#echo $TRAVIS_REPO_SLUG
 docker build -f Dockerfile -t "$TRAVIS_REPO_SLUG":$TAG .
 docker push "$TRAVIS_REPO_SLUG"
